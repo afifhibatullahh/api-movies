5f5551a0d814a34e0c4f6b988faaa1b7438130d7
afifhibatullahh <afifhibatullah59@gmail.com>
2022-07-23 09:12:26 +0700

    update decode jwt

--- app/Helpers/jwt_helper.php
+++ app/Helpers/jwt_helper.php
@@ -2,11 +2,12 @@
 
 use App\Models\ModelUser;
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 function getJWT($autentikasiHeader)
 {
     if (is_null($autentikasiHeader)) {
-        throw new Exception('Authentikasi JWT Gagal');
+        throw new Exception('jwt authentication failed');
     }
     return explode(" ", $autentikasiHeader)[1];
 }
@@ -14,7 +15,7 @@ function getJWT($autentikasiHeader)
 function validateJWT($encodedToken)
 {
     $key = getenv('JWT_SECRET_KEY');
-    $decodedToken = JWT::decode($encodedToken, $key, ['HS256']);
+    $decodedToken = JWT::decode($encodedToken, new Key($key, 'HS256'));
     $modelUser = new ModelUser();
 
     $modelUser->getUsername($decodedToken->username);
